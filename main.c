0315414807c7ca38ab330c91dd41eac2be34d1cf
wilfried-oss <davowilfried3@gmail.com>
2022-06-30 11:43:07 +0100

    Renaming of Main file

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int number = 0;
+
+    printf("Give me positive number please : ");
+    scanf("%d", &number);
+
+    while (number < 0)
+    {
+        printf("Give me positive number please : ");
+        scanf("%d", &number);
+    }
+
+    // do
+    // {
+    //     printf("Give me positive number please : ");
+    //     scanf("%d", &number);
+
+    // } while (number < 0);
+
+    return 0;
+}
